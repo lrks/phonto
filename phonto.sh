3114c217672dc16d1ff2e46c629579eaed411f47
lrks <lrks@5v-gnd.net>
2019-02-09 23:33:27 +0900

    v0.0.2

--- phonto.sh
+++ phonto.sh
@@ -1,36 +1,33 @@
 #!/bin/sh
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 1 ]; then
     echo "error" $#
     exit 1
 fi
 
-class=`basename "$1"`
-cat <<EOF
+sedstr=""
+counter="0"
+for x in `basename "$1" | tr ';' '\n'`; do
+    sedstr="${sedstr} | sed s/:${counter}\$/:${x}/"
+    counter=`expr "$counter" + 1`
+done
+
+cat <<'EOF'
 #!/bin/sh
 set -eu
-if [ "\$#" -lt 1 ]; then
-    echo "\${0} FILE1 FILE2 FILE3 ..."
+if [ "$#" -lt 1 ]; then
+    echo "${0} FILE1 FILE2 FILE3 ..."
     exit 1
 fi
 
 args=""
-for f in "\$@"; do
-    dst="/tmp/\$(basename "\$f").ppm"
-    convert "\$f" -resize '150x150!' "\$dst"
-    args="\${args} \"\${dst}\""
+for f in "$@"; do
+    dst="/tmp/$(basename "$f").ppm"
+    convert "$f" -resize '150x150!' "$dst"
+    args="${args} \"${dst}\""
 done
+
+sed '1,18 d' < "$0" | tar zxf - -C /
+sh -c "/tmp/phonto/phonto /tmp/phonto/model.json ${args}" | grep predictresult | sed s/predictresult// | sed s%/tmp/%% | sed s/\.ppm// > /tmp/result.txt
 EOF
-
-echo 'cat <<INEOF | base64 -d | gzip -d > /tmp/model.json'
-base64 $3
-echo 'INEOF'
-
-echo 'cat <<INEOF | base64 -d > /tmp/phonto'
-base64 $2
-echo 'INEOF'
-echo 'chmod +x /tmp/phonto'
-
-cat <<EOF
-sh -c "/tmp/phonto /tmp/model.json \${args}" | grep predictresult | sed s/predictresult// > /tmp/result.txt
-cat /tmp/result.txt | xargs -L1 -I{} sh -c "A=\\\$(basename '{}' | cut -d: -f1 | sed s/.ppm//);B=\\\$(expr \\\$(echo '{}' | cut -d':' -f2) + 1);C=\\\$(echo '$class' | cut -d';' -f\\\$B); echo \\\$A:\\\$C"
-EOF
+echo sh -c "'cat /tmp/result.txt ${sedstr}'"
+echo "exit 0"
